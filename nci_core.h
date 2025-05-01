ab820ae026dc2d664df9edc420a54fc6863b87a5
Praveen Balakrishnan <praveen.balakrishnan@magd.ox.ac.uk>
2025-05-01 23:44:01 +0100

    NCI_CORE_INIT command initial support

--- nci_core.h
+++ nci_core.h
@@ -33,6 +33,25 @@
 #define NCI_CORE_RESET_V11                  0x11
 #define NCI_CORE_RESET_V20                  0x20
 
+#define NCI_CORE_O0_DISCOVER_FREQ           0b00000001
+#define NCI_CORE_O0_MULTICONFIG             0b00000010
+#define NCI_CORE_O0_HCI_NETWORK             0b00001000
+#define NCI_CORE_O0_ACTIVE_COMM             0b00010000
+
+#define NCI_CORE_O1_TECH_ROUTING            0b00000010
+#define NCI_CORE_O1_PROTOCOL_ROUTING        0b00000100
+#define NCI_CORE_O1_AID_ROUTING             0b00001000
+#define NCI_CORE_O1_SYSTEM_CODE_ROUTING     0b00010000
+#define NCI_CORE_O1_APDU_ROUTING            0b00100000
+#define NCI_CORE_O1_FORCED_NFCEE_ROUTING    0b01000000
+
+#define NCI_CORE_O2_BATTERY_OFF_STATE       0b00000001
+#define NCI_CORE_O2_SWITCH_OFF_STATE        0b00000010
+#define NCI_CORE_O2_SWITCH_ON_SUBMODE_STATE 0b00000100
+#define NCI_CORE_O2_RF_CONF_SWITCH_OFF      0b00001000
+
+
+
 extern hf_register_info nci_core_hf[];
 
 void handle_nci_core(int oid, int pkt_type, proto_tree* tree, tvbuff_t *tvb, int* hoffset);
